1b77d974fef6895914696a00eb82232d93ad1193
Jorge Lopez <jlopez1029@hotmail.com>
2025-09-04 22:16:32 -0500

    feat: Add example in TicketController

--- src/main/java/io/paymeter/assessment/domain/model/request/TicketRequest.java
+++ src/main/java/io/paymeter/assessment/domain/model/request/TicketRequest.java
@@ -2,6 +2,7 @@ package io.paymeter.assessment.domain.model.request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
@@ -21,10 +22,12 @@ import java.time.OffsetDateTime;
 @AllArgsConstructor
 public class TicketRequest {
 
+    @Schema(description = "Parking Id", example = "P00123")
     @NotBlank(message = "parkingId cannot be empty")
     @Size(min = 1, max = 30, message = "ParkingId must not exceed 30 characters")
     private String parkingId;
 
+    @Schema(description = "Date of entry to the establishment", example = "2025-09-03T21:45:00.000")
     @NotNull(message = "Entry date (from) cannot be null")
     @Pattern(
             regexp = "^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}(\\.\\d{3})?(Z|[+-]\\d{2}:\\d{2})?$",
@@ -32,6 +35,7 @@ public class TicketRequest {
     )
     private String from;
 
+    @Schema(description = "Departure date to the establishment", example = "2025-09-03T21:45:00.000")
     @NotNull(message = "Checkout date (to) cannot be null")
     @Pattern(
             regexp = "^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}(\\.\\d{3})?(Z|[+-]\\d{2}:\\d{2})?$",
